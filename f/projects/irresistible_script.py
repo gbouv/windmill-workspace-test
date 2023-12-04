3f07785ec53065bbfef8ed473dd9b79a3a13067c
admin <admin@windmill.dev>
2023-12-04 13:47:14 +0000

    Script 'f/projects/irresistible_script' deployed

--- f/projects/irresistible_script.py
+++ f/projects/irresistible_script.py
@@ -5,5 +5,7 @@ def main():
     return {
         "windmill_content_type": "text/csv",
         # "windmill_status_code": 200,
-        "result": "Hello;World"
+        "result": { 
+            "vale": "Hello;World"
+        }
     }
