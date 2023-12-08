50c7712c073e638faeec1862df8ba1c54abbdc77
admin <admin@windmill.dev>
2023-12-08 18:30:50 +0000

    Script 'f/projects/cozy_script' deployed

--- f/projects/cozy_script.go
+++ f/projects/cozy_script.go
@@ -2,9 +2,11 @@ package inner
 
 import (
 	"fmt"
+  "time"
 )
 
-func main() (interface{}, error) {
+func main(sleep_sec: int) (interface{}, error) {
 	fmt.Println("Hello, Cozy script!")
+  time.Sleep(time.Duration(sleep_sec) * time.Second)
 	return "This is cozy", nil
 }
